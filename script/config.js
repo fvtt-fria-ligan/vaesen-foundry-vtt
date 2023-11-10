cd4b9142e4e16e4d64f37c6b01e0690c2dcc4e0d
Cussa de Souza <pcussa@22cans.com>
2023-11-10 14:28:36 -0300

    feat(talent): new bonus types

--- script/config.js
+++ script/config.js
@@ -13,4 +13,18 @@ vaesen.skills = {
     inspiration: "SKILL.INSPIRATION",
     manipulation: "SKILL.MANIPULATION",
     observation: "SKILL.OBSERVATION",
-}
+};
+
+vaesen.bonusType = {
+    none: "BONUS_TYPE.NONE",
+    skill: "BONUS_TYPE.SKILL",
+    ignoreConditionSkill: "BONUS_TYPE.IGNORE_CONDITIONS_SKILL",
+    ignoreConditionPhysical: "BONUS_TYPE.IGNORE_CONDITIONS_PHYSICAL",
+    ignoreConditionMental: "BONUS_TYPE.IGNORE_CONDITIONS_MENTAL",
+    damage: "BONUS_TYPE.DAMAGE",
+    fear: "BONUS_TYPE.FEAR"
+};
+
+vaesen.bonusTypeRequiresSkill = ["skill", "ignoreConditionSkill", "damage"];
+vaesen.bonusTypeRequiresBonus = ["skill", "damage", "fear"];
+vaesen.bonusTypeDamageSkills = ["closeCombat", "force", "rangedCombat"];
