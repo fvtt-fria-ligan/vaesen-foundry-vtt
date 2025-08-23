02b59876e5b0ff8c227437b5027c58baadec2a65
Cussa Mitre <cussa.mitre@gmail.com>
2025-08-23 18:48:29 +0100

    ✨ Closes #139 - Allow critical injury to affect Fear test

--- script/config.js
+++ script/config.js
@@ -15,6 +15,11 @@ vaesen.skills = {
     observation: "SKILL.OBSERVATION",
 };
 
+vaesen.criticalInjuryOptions = {
+    ...vaesen.skills,
+    "fear": "BONUS_TYPE.FEAR",
+};
+
 vaesen.bonusType = {
     none: "BONUS_TYPE.NONE",
     skill: "BONUS_TYPE.SKILL",
@@ -38,16 +43,16 @@ vaesen.bonusTypeDamageSkills = ["closeCombat", "force", "rangedCombat"];
 
 vaesen.bonusTypeDamageSkillsOptions = {
     closeCombat: "SKILL.CLOSE_COMBAT",
-    rangedCombat: "SKILL.RANGED_COMBAT", 
+    rangedCombat: "SKILL.RANGED_COMBAT",
     force: "SKILL.FORCE"
 }
 
 // select option set up
 vaesen.attackAttributeOptions = {
-        might: "ATTRIBUTE.MIGHT_ROLL",
-        bodyControl:"ATTRIBUTE.BODY_CONTROL_ROLL", 
-        magic: "ATTRIBUTE.MAGIC_ROLL",
-        manipulation:"ATTRIBUTE.MANIPULATION_ROLL"
+    might: "ATTRIBUTE.MIGHT_ROLL",
+    bodyControl: "ATTRIBUTE.BODY_CONTROL_ROLL",
+    magic: "ATTRIBUTE.MAGIC_ROLL",
+    manipulation: "ATTRIBUTE.MANIPULATION_ROLL"
 }
 
 vaesen.injuryTypeOptions = {
@@ -68,7 +73,7 @@ vaesen.magicCategoryOptions = {
 }
 
 vaesen.upgradeCategory = {
-         facility: "UPGRADE.FACILITY",
-        contact: "UPGRADE.CONTACT",
-        personnel: "UPGRADE.PERSONNEL"
+    facility: "UPGRADE.FACILITY",
+    contact: "UPGRADE.CONTACT",
+    personnel: "UPGRADE.PERSONNEL"
 }
